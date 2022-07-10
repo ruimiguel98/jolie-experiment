2e25a92e8cbfe403f553793714be41f4c0b80a7a
ruimiguel98 <ruimiguel98@hotmail.com>
2022-07-10 16:17:12 +0100

    make payment service first draft in spring boot

--- Services/spring-boot/services/payment-service/src/main/java/org/example/controller/PaymentController.java
+++ Services/spring-boot/services/payment-service/src/main/java/org/example/controller/PaymentController.java
@@ -1,2 +1,30 @@
-package org.example.controller;public class PaymentController {
+package org.example.controller;
+
+import org.example.entity.Payment;
+import org.example.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/addPaymentInfo")
+    public Payment savePayment(@RequestBody Payment payment) {
+        return paymentService.savePayment(payment);
+    }
+
+    @GetMapping("/getSavedPaymentInfoList")
+    public List<Payment> fetchPaymentList() {
+        return paymentService.fetchPaymentList();
+    }
+
+    @DeleteMapping("/deletePaymentInfo")
+    public String deletePaymentById(@RequestParam("id") Long paymentId) {
+        paymentService.deletePaymentById(paymentId);
+        return "Payment method/info deleted successfully";
+    }
 }
--- Services/spring-boot/services/payment-service/src/main/java/org/example/repository/PaymentRepository.java
+++ Services/spring-boot/services/payment-service/src/main/java/org/example/repository/PaymentRepository.java
@@ -1,2 +1,9 @@
-package org.example.repository;public interface PaymentRepository {
+package org.example.repository;
+
+import org.example.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
 }
