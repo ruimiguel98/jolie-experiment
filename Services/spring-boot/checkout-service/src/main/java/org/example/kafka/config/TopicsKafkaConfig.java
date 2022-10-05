722301523152f660926997487101b4b869eaf331
ruimiguel98 <ruimiguel98@hotmail.com>
2022-10-05 19:57:08 +0100

    refactoring direcotry structure on Jolie project

--- Services/spring-boot/checkout-service/src/main/java/org/example/kafka/config/TopicsKafkaConfig.java
+++ Services/spring-boot/checkout-service/src/main/java/org/example/kafka/config/TopicsKafkaConfig.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
