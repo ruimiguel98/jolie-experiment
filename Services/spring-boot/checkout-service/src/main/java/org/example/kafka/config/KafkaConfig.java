64f879ce143abb1486d25acf7f02cf361d6b142e
ruimiguel98 <ruimiguel98@hotmail.com>
2022-10-13 16:54:02 +0100

    checkout service WIP

--- Services/spring-boot/checkout-service/src/main/java/org/example/kafka/config/KafkaConfig.java
+++ Services/spring-boot/checkout-service/src/main/java/org/example/kafka/config/KafkaConfig.java
@@ -1,10 +1,12 @@
 package org.example.kafka.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
@@ -12,125 +14,40 @@ import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import java.time.Duration;
 
 @Configuration
-@Slf4j
 public class KafkaConfig {
 
-    @Value("${kafka.topic.reply-payment}")
-    private String REPLY_TOPIC_PAYMENT;
-    @Value("${kafka.topic.reply-email}")
-    private String REPLY_TOPIC_EMAIL;
-    @Value("${kafka.topic.reply-order}")
-    private String REPLY_TOPIC_ORDER;
-    @Value("${kafka.topic.reply-cart}")
-    private String REPLY_TOPIC_CART;
-
-    @Value("${kafka.consumer-group-payment}")
-    private String CONSUMER_GROUP_PAYMENT;
-    @Value("${kafka.consumer-group-email}")
-    private String CONSUMER_GROUP_EMAIL;
-    @Value("${kafka.consumer-group-order}")
-    private String CONSUMER_GROUP_ORDER;
-    @Value("${kafka.consumer-group-cart}")
-    private String CONSUMER_GROUP_CART;
-
-
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /***********************************                  PAYMENT               ***********************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    @Bean //register and configure replying kafka template
-    public ReplyingKafkaTemplate<String, String, String> replyingTemplateForPayment(
+    @Bean
+    public ReplyingKafkaTemplate<String, String, String> replyingTemplate(
             ProducerFactory<String, String> pf,
-            ConcurrentMessageListenerContainer<String, String> repliesContainerForPayment) {
-        ReplyingKafkaTemplate<String, String, String> replyTemplate = new ReplyingKafkaTemplate<>(pf, repliesContainerForPayment);
-        replyTemplate.setDefaultReplyTimeout(Duration.ofSeconds(10));
-        replyTemplate.setSharedReplyTopic(true);
-        return replyTemplate;
+            ConcurrentMessageListenerContainer<String, String> repliesContainer) {
+
+        return new ReplyingKafkaTemplate<>(pf, repliesContainer);
     }
 
-    @Bean //register ConcurrentMessageListenerContainer bean
-    public ConcurrentMessageListenerContainer<String, String> repliesContainerForPayment(
+    @Bean
+    public ConcurrentMessageListenerContainer<String, String> repliesContainer(
             ConcurrentKafkaListenerContainerFactory<String, String> containerFactory) {
-        ConcurrentMessageListenerContainer<String, String> repliesContainerForPayment = containerFactory.createContainer(REPLY_TOPIC_PAYMENT);
-        repliesContainerForPayment.getContainerProperties().setGroupId(CONSUMER_GROUP_PAYMENT);
-        repliesContainerForPayment.setAutoStartup(false);
-        return repliesContainerForPayment;
+
+        ConcurrentMessageListenerContainer<String, String> repliesContainer =
+                containerFactory.createContainer("kReplies2");
+        repliesContainer.getContainerProperties().setGroupId("repliesGroup2");
+        repliesContainer.setAutoStartup(false);
+        return repliesContainer;
     }
 
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /***********************************                   CART                 ***********************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    @Bean //register and configure replying kafka template
-    public ReplyingKafkaTemplate<String, String, String> replyingTemplateForCart(
-            ProducerFactory<String, String> pf,
-            ConcurrentMessageListenerContainer<String, String> repliesContainerForCart) {
-        ReplyingKafkaTemplate<String, String, String> replyTemplate = new ReplyingKafkaTemplate<>(pf, repliesContainerForCart);
-        replyTemplate.setDefaultReplyTimeout(Duration.ofSeconds(10));
-        replyTemplate.setSharedReplyTopic(true);
-        return replyTemplate;
+    @Bean
+    public NewTopic kRequests() {
+        return TopicBuilder.name("kRequests2")
+                .partitions(10)
+                .replicas(2)
+                .build();
     }
 
-    @Bean //register ConcurrentMessageListenerContainer bean
-    public ConcurrentMessageListenerContainer<String, String> repliesContainerForCart(
-            ConcurrentKafkaListenerContainerFactory<String, String> containerFactory) {
-        ConcurrentMessageListenerContainer<String, String> repliesContainerForCart = containerFactory.createContainer(REPLY_TOPIC_CART);
-        repliesContainerForCart.getContainerProperties().setGroupId(CONSUMER_GROUP_CART);
-        repliesContainerForCart.setAutoStartup(false);
-        return repliesContainerForCart;
-    }
-
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /***********************************                   ORDER                ***********************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    @Bean //register and configure replying kafka template
-    public ReplyingKafkaTemplate<String, String, String> replyingTemplateForOrder(
-            ProducerFactory<String, String> pf,
-            ConcurrentMessageListenerContainer<String, String> repliesContainerForOrder) {
-        ReplyingKafkaTemplate<String, String, String> replyTemplate = new ReplyingKafkaTemplate<>(pf, repliesContainerForOrder);
-        replyTemplate.setDefaultReplyTimeout(Duration.ofSeconds(10));
-        replyTemplate.setSharedReplyTopic(true);
-        return replyTemplate;
-    }
-
-    @Bean //register ConcurrentMessageListenerContainer bean
-    public ConcurrentMessageListenerContainer<String, String> repliesContainerForOrder(
-            ConcurrentKafkaListenerContainerFactory<String, String> containerFactory) {
-        ConcurrentMessageListenerContainer<String, String> repliesContainerForOrder = containerFactory.createContainer(REPLY_TOPIC_ORDER);
-        repliesContainerForOrder.getContainerProperties().setGroupId(CONSUMER_GROUP_ORDER);
-        repliesContainerForOrder.setAutoStartup(false);
-        return repliesContainerForOrder;
-    }
-
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /***********************************                   EMAIL                ***********************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    /**************************************************************************************************************/
-    @Bean //register and configure replying kafka template
-    public ReplyingKafkaTemplate<String, String, String> replyingTemplateForEmail(
-            ProducerFactory<String, String> pf,
-            ConcurrentMessageListenerContainer<String, String> repliesContainerForEmail) {
-        ReplyingKafkaTemplate<String, String, String> replyTemplate = new ReplyingKafkaTemplate<>(pf, repliesContainerForEmail);
-        replyTemplate.setDefaultReplyTimeout(Duration.ofSeconds(10));
-        replyTemplate.setSharedReplyTopic(true);
-        return replyTemplate;
-    }
-
-    @Bean //register ConcurrentMessageListenerContainer bean
-    public ConcurrentMessageListenerContainer<String, String> repliesContainerForEmail(
-            ConcurrentKafkaListenerContainerFactory<String, String> containerFactory) {
-        ConcurrentMessageListenerContainer<String, String> repliesContainerForEmail = containerFactory.createContainer(REPLY_TOPIC_EMAIL);
-        repliesContainerForEmail.getContainerProperties().setGroupId(CONSUMER_GROUP_EMAIL);
-        repliesContainerForEmail.setAutoStartup(false);
-        return repliesContainerForEmail;
+    @Bean
+    public NewTopic kReplies() {
+        return TopicBuilder.name("kReplies2")
+                .partitions(10)
+                .replicas(2)
+                .build();
     }
 }
--- Services/spring-boot/checkout-service/src/main/java/org/example/service/CheckoutService.java
+++ Services/spring-boot/checkout-service/src/main/java/org/example/service/CheckoutService.java
@@ -13,10 +13,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
@@ -26,33 +28,16 @@ public class CheckoutService {
     CheckoutCRUD checkoutCRUD;
 
     @Autowired
-    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForCart;
+    private ReplyingKafkaTemplate<String, String, String> replyingTemplate;
 
-    @Autowired
-    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForPayment;
-
-    @Autowired
-    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForOrder;
-
-    @Autowired
-    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForEmail;
-
-    @Value("${kafka.topic.request-cart}")
-    private String requestCartTotalTopic;
-    @Value("${kafka.topic.request-cart}")
-    private String replyCartTotalTopic;
-    @Value("${kafka.topic.request-payment}")
-    private String requestPaymentProcessTopic;
-    @Value("${kafka.topic.reply-payment}")
-    private String replyPaymentProcessTopic;
-    @Value("${kafka.topic.request-order}")
-    private String requestOrderTopic;
-    @Value("${kafka.topic.reply-order}")
-    private String replyOrderTopic;
-    @Value("${kafka.topic.request-email}")
-    private String requestEmailTopic;
-    @Value("${kafka.topic.request-email}")
-    private String replyEmailTopic;
+//    @Autowired
+//    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForPayment;
+//
+//    @Autowired
+//    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForOrder;
+//
+//    @Autowired
+//    private ReplyingKafkaTemplate<String, String, String> replyingTemplateForEmail;
 
     public Checkout performCheckout(CreateCheckoutForm createCheckoutForm) throws Exception {
 
@@ -61,62 +46,59 @@ public class CheckoutService {
         ReplyCartTotal topicResponseCartTotal = sendMessageWaitReplyCartTotalTopic(cartId);
         log.info("The cart total is " + topicResponseCartTotal.toString());
 
-        //----------------------------- 1. WITHDRAWAL PROVIDED BANK ACCOUNT --------------------------------
-        String cardNumber = createCheckoutForm.getCardNumber();
-        String cardCVV = createCheckoutForm.getCardCVV();
-        Double amountToWithdrawal = topicResponseCartTotal.getCartTotalPrice();
-//        Double amountToWithdrawal = 22.0;
-        ReplyPaymentProcess topicResponsePaymentProcess = sendMessageWaitReplyPaymentProcessTopic(cardNumber, cardCVV, amountToWithdrawal);
-        log.info("The payment process returned " + topicResponsePaymentProcess.toString());
+//        //----------------------------- 1. WITHDRAWAL PROVIDED BANK ACCOUNT --------------------------------
+//        String cardNumber = createCheckoutForm.getCardNumber();
+//        String cardCVV = createCheckoutForm.getCardCVV();
+//        Double amountToWithdrawal = topicResponseCartTotal.getCartTotalPrice();
+////        Double amountToWithdrawal = 22.0;
+//        ReplyPaymentProcess topicResponsePaymentProcess = sendMessageWaitReplyPaymentProcessTopic(cardNumber, cardCVV, amountToWithdrawal);
+//        log.info("The payment process returned " + topicResponsePaymentProcess.toString());
+//
+//        //----------------------------- 2. PLACE THE ORDER --------------------------------
+//        String userId = createCheckoutForm.getUserId().toString();
+//        String status = "CREATED";
+//        String addressToShip = createCheckoutForm.getOrder().getAddressToShip();
+//        HashMap<String, String> orderProducts = new HashMap<>();
+//        for (Product product : createCheckoutForm.getOrder().getProducts()) {
+//            orderProducts.put(product.getId(), product.getQuantity().toString());
+//        }
+//
+//        ReplyOrder topicResponseOrder = sendMessageWaitReplyOrderTopic(userId, status, addressToShip, orderProducts);
+//        log.info("Order placed with ID " + topicResponseOrder.getOrderId());
+//
+//        //----------------------------- 3. SEND ORDER PLACED EMAIL --------------------------------
+//        ReplyEmail topicResponseEmail = sendMessageWaitReplyEmailTopic("ORDER PLACED",
+//                "Your order has been placed",
+//                "shipping@ourcompany.com",
+//                "test@gmail.com",
+//                "22/12/2022");
+//        log.info("The email has been sent with the order placed confirmation with ID " + topicResponseEmail.getId());
+//
+//        //----------------------------- 4. UPDATE CHECKOUT RECORDS DATABASE --------------------------------
+//        UUID checkoutUUID = UUID.randomUUID();
+//        Checkout checkout = new Checkout(
+//                checkoutUUID,
+//                UUID.fromString(topicResponseOrder.getOrderId()),
+//                createCheckoutForm.getCartId()
+//        );
+////        checkoutCRUD.save(checkout);
 
-        //----------------------------- 2. PLACE THE ORDER --------------------------------
-        String userId = createCheckoutForm.getUserId().toString();
-        String status = "CREATED";
-        String addressToShip = createCheckoutForm.getOrder().getAddressToShip();
-        HashMap<String, String> orderProducts = new HashMap<>();
-        for (Product product : createCheckoutForm.getOrder().getProducts()) {
-            orderProducts.put(product.getId(), product.getQuantity().toString());
-        }
-
-        ReplyOrder topicResponseOrder = sendMessageWaitReplyOrderTopic(userId, status, addressToShip, orderProducts);
-        log.info("Order placed with ID " + topicResponseOrder.getOrderId());
-
-        //----------------------------- 3. SEND ORDER PLACED EMAIL --------------------------------
-        ReplyEmail topicResponseEmail = sendMessageWaitReplyEmailTopic("ORDER PLACED",
-                "Your order has been placed",
-                "shipping@ourcompany.com",
-                "test@gmail.com",
-                "22/12/2022");
-        log.info("The email has been sent with the order placed confirmation with ID " + topicResponseEmail.getId());
-
-        //----------------------------- 4. UPDATE CHECKOUT RECORDS DATABASE --------------------------------
-        UUID checkoutUUID = UUID.randomUUID();
-        Checkout checkout = new Checkout(
-                checkoutUUID,
-                UUID.fromString(topicResponseOrder.getOrderId()),
-                createCheckoutForm.getCartId()
-        );
-//        checkoutCRUD.save(checkout);
-
-        return checkout;
+        return null;
 
     }
 
     public ReplyCartTotal sendMessageWaitReplyCartTotalTopic(String cartId) throws Exception {
-
-        // THIS IS EXTREMELY IMPORTANT
-        this.replyingTemplateForCart.setSharedReplyTopic(true);
-
         try {
             RequestCartTotal topicRequest = RequestCartTotal
                     .builder()
                     .id(cartId)
                     .build();
 
-            ProducerRecord<String, String> record = new ProducerRecord<>(requestCartTotalTopic, new Gson().toJson(topicRequest));
-//            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyCartTotalTopic.getBytes()));
-            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForCart.sendAndReceive(record);
-            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
+            ProducerRecord<String, String> record = new ProducerRecord<>("kRequests2",  new Gson().toJson(topicRequest));
+            RequestReplyFuture<String, String, String> replyFuture = replyingTemplate.sendAndReceive(record);
+            SendResult<String, String> sendResult = replyFuture.getSendFuture().get(10, TimeUnit.SECONDS);
+            System.out.println("Sent ok: " + sendResult.getRecordMetadata());
+            ConsumerRecord<String, String> consumerRecord = replyFuture.get(10, TimeUnit.SECONDS);
 
             ReplyCartTotal topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyCartTotal.class);;
             return topicResponse;
@@ -126,86 +108,74 @@ public class CheckoutService {
         }
     }
 
-    public ReplyPaymentProcess sendMessageWaitReplyPaymentProcessTopic(String cardNumber, String CVV, Double amountToWithdrawal) throws Exception {
-
-        // THIS IS EXTREMELY IMPORTANT
-        this.replyingTemplateForPayment.setSharedReplyTopic(true);
-
-        try {
-            RequestPaymentProcess topicRequest = RequestPaymentProcess
-                    .builder()
-                    .cardNumber(cardNumber)
-                    .CVV(CVV)
-                    .amountToWithdrawal(amountToWithdrawal)
-                    .build();
-
-            ProducerRecord<String, String> record = new ProducerRecord<>(requestPaymentProcessTopic, new Gson().toJson(topicRequest));
-//            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyPaymentProcessTopic.getBytes()));
-
-            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForPayment.sendAndReceive(record);
-            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
-
-            ReplyPaymentProcess topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyPaymentProcess.class);;
-            return topicResponse;
-
-        } catch (Exception e) {
-            throw new Exception();
-        }
-    }
-
-    public ReplyOrder sendMessageWaitReplyOrderTopic(String userId, String status, String addressToShip, HashMap<String, String> orderProducts) throws Exception {
-
-        // THIS IS EXTREMELY IMPORTANT
-        this.replyingTemplateForOrder.setSharedReplyTopic(true);
-
-        try {
-            RequestOrder topicRequest = RequestOrder
-                    .builder()
-                    .userId(UUID.fromString(userId))
-                    .status(status)
-                    .addressToShip(addressToShip)
-                    .products(orderProducts)
-                    .build();
-
-            ProducerRecord<String, String> record = new ProducerRecord<>(requestOrderTopic, new Gson().toJson(topicRequest));
-            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyOrderTopic.getBytes()));
-            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForOrder.sendAndReceive(record);
-            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
-
-            ReplyOrder topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyOrder.class);;
-            return topicResponse;
-
-        } catch (Exception e) {
-            throw new Exception();
-        }
-    }
-
-    public ReplyEmail sendMessageWaitReplyEmailTopic(String subject, String message, String fromEmail, String toEmail, String sentDate) throws Exception {
-
-        // THIS IS EXTREMELY IMPORTANT
-        this.replyingTemplateForEmail.setSharedReplyTopic(true);
-
-        try {
-            RequestEmail topicRequest = RequestEmail
-                    .builder()
-                    .subject(subject)
-                    .message(message)
-                    .fromEmail(fromEmail)
-                    .toEmail(toEmail)
-                    .sentDate(sentDate)
-                    .build();
-
-            ProducerRecord<String, String> record = new ProducerRecord<>(requestEmailTopic, new Gson().toJson(topicRequest));
-            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyEmailTopic.getBytes()));
-            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForEmail.sendAndReceive(record);
-            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
-
-            ReplyEmail topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyEmail.class);;
-            return topicResponse;
-
-        } catch (Exception e) {
-            throw new Exception();
-        }
-    }
+//    public ReplyPaymentProcess sendMessageWaitReplyPaymentProcessTopic(String cardNumber, String CVV, Double amountToWithdrawal) throws Exception {
+//        try {
+//            RequestPaymentProcess topicRequest = RequestPaymentProcess
+//                    .builder()
+//                    .cardNumber(cardNumber)
+//                    .CVV(CVV)
+//                    .amountToWithdrawal(amountToWithdrawal)
+//                    .build();
+//
+//            ProducerRecord<String, String> record = new ProducerRecord<>(requestPaymentProcessTopic, new Gson().toJson(topicRequest));
+////            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyPaymentProcessTopic.getBytes()));
+//
+//            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForPayment.sendAndReceive(record);
+//            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
+//
+//            ReplyPaymentProcess topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyPaymentProcess.class);;
+//            return topicResponse;
+//
+//        } catch (Exception e) {
+//            throw new Exception();
+//        }
+//    }
+//
+//    public ReplyOrder sendMessageWaitReplyOrderTopic(String userId, String status, String addressToShip, HashMap<String, String> orderProducts) throws Exception {
+//        try {
+//            RequestOrder topicRequest = RequestOrder
+//                    .builder()
+//                    .userId(UUID.fromString(userId))
+//                    .status(status)
+//                    .addressToShip(addressToShip)
+//                    .products(orderProducts)
+//                    .build();
+//
+//            ProducerRecord<String, String> record = new ProducerRecord<>(requestOrderTopic, new Gson().toJson(topicRequest));
+//            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyOrderTopic.getBytes()));
+//            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForOrder.sendAndReceive(record);
+//            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
+//
+//            ReplyOrder topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyOrder.class);;
+//            return topicResponse;
+//
+//        } catch (Exception e) {
+//            throw new Exception();
+//        }
+//    }
+//
+//    public ReplyEmail sendMessageWaitReplyEmailTopic(String subject, String message, String fromEmail, String toEmail, String sentDate) throws Exception {
+//        try {
+//            RequestEmail topicRequest = RequestEmail
+//                    .builder()
+//                    .subject(subject)
+//                    .message(message)
+//                    .fromEmail(fromEmail)
+//                    .toEmail(toEmail)
+//                    .sentDate(sentDate)
+//                    .build();
+//
+//            ProducerRecord<String, String> record = new ProducerRecord<>(requestEmailTopic, new Gson().toJson(topicRequest));
+//            record.headers().add(new RecordHeader(KafkaHeaders.REPLY_TOPIC, replyEmailTopic.getBytes()));
+//            RequestReplyFuture<String, String, String> sendAndReceive = this.replyingTemplateForEmail.sendAndReceive(record);
+//            ConsumerRecord<String, String> consumerRecord = sendAndReceive.get();
+//
+//            ReplyEmail topicResponse = new Gson().fromJson(consumerRecord.value(), ReplyEmail.class);;
+//            return topicResponse;
+//
+//        } catch (Exception e) {
+//            throw new Exception();
+//        }
+//    }
 
 }
